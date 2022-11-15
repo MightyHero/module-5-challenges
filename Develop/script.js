401e7eb0cdad5dc73ba538b73c9abecf0853cf92
MightyHero <115051723+MightyHero@users.noreply.github.com>
2022-11-14 23:09:13 -0600

    added source for javascript to html

--- Develop/script.js
+++ Develop/script.js
@@ -1,3 +1,30 @@
+var currentDay = document.querySelector("#currentDay");
+var date = moment().format("dddd, MMM Do YYYY");
+var timeBlockContainer = $(".container");
+
+function pageLoad() {
+    currentDay.textContent = date;
+}
+
+function addTimeBlock() {
+    var currentHour = moment().format("H");
+    for (let i = 0; i < 10; i++) {
+        var nextRow = $("<div>");
+        nextRow.attr("class", "row");
+        var infoBlock = $("<p>");
+        infoBlock.attr("class", "hour");
+        infoBlock.text (
+            moment()
+                .hour(i + 8)
+                .format("hA")
+        );
+        nextRow.append(infoBlock);
+        timeBlockContainer.append(nextRow); 
+    }
+}
+addTimeBlock();
+pageLoad();
+
 moment().format('MMMM Do YYYY, h:mm:ss a');
 moment().format('dddd');
 moment().format("MMM Do YY");
